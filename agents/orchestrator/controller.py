1a9c1f92caf52b3720f0dedce34a8bd2535da71b
wallace0669 <seanshue0423@gamil.com>
2025-12-26 20:16:59 +0800

    feat: adding dependent info in orchestrator

--- agents/orchestrator/controller.py
+++ agents/orchestrator/controller.py
@@ -6,6 +6,15 @@ from agentcore import LLMClient, BaseGraph
 from agents.orchestrator.schema import OrchestratorSchema
 from agents.orchestrator.tool import OrchestratorTool
 
+# dependent controller
+from agents.diagnostic_agent.controller import DiagnosticAgent
+from agents.questioning_agent.controller import QuestioningAgent
+from agents.integration_agent.controller import IntegrationAgent
+
+# dependent schema
+from agents.diagnostic_agent.schema import DiagnosticAgentSchema
+from agents.questioning_agent.schema import QuestioningAgentSchema
+from agents.integration_agent.schema import IntegrationAgentSchema
 
 class Orchestrator(BaseGraph):
     """Agent for orchestrator."""
@@ -18,10 +27,29 @@ class Orchestrator(BaseGraph):
         self.conditional_edges = OrchestratorSchema.conditional_edges
         self.direct_edges = OrchestratorSchema.direct_edges
 
-        # --- load dependent graphs ---
-        DEPENDENT_GRAPHS = {}
-        self.subgraphs = {k: v() for k, v in DEPENDENT_GRAPHS.items()}
-        self.state_mapping = OrchestratorSchema.state_mapping
+        # --- load dependent graphs and schemas ---
+        DEPENDENT_GRAPHS_AND_SCHEMA = {
+            "diagnostic_agent": {
+                "controller": DiagnosticAgent,
+                "schema": DiagnosticAgentSchema
+            },
+            "questioning_agent": {
+                "controller": QuestioningAgent,
+                "schema": QuestioningAgentSchema
+            },
+            "integration_agent": {
+                "controller": IntegrationAgent,
+                "schema": IntegrationAgentSchema
+            },
+        }
+
+        self.subgraphs = {}
+        for k, v in DEPENDENT_GRAPHS_AND_SCHEMA.items():
+            subgraph_instance = v["controller"](llm_client)
+            self.subgraphs[k] = self.register_subgraph(
+                subgraph_instance.compile(),
+                v["schema"].state_mapping
+            )
         
         # --- load tools ---
         self.tools = OrchestratorTool(llm_client)
